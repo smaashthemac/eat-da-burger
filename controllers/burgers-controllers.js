98d3f6fa763fac01ac3d1ef0270c3b98e45da84d
Christ <smaashthemac@gmail.com>
2016-12-15 22:36:06 -0600

    updated burger controller and server. have copied over code from cats app, but haven't checked to see if anything works yet. fingers crossed.

--- controllers/burgers-controllers.js
+++ controllers/burgers-controllers.js
@@ -3,3 +3,26 @@ var express = require("express");
 var burger = require("../models/burger.js");
 
 // create all routes
+router.get("/", function(req, res) {
+	res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+	burger.all(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render("index", hbsObject);
+	});
+});
+
+router.post("/burgers/create", function(req, res) {
+	burger.create([
+		"burgerName"
+		], [
+		req.body.burgerName
+		], function() {
+			res.redirect("/burgers");
+		});
+});
--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 var express = require("express");
 var methodOverride = require("mothod-override");
 var bodyParser = require("body-parser");
+
+var port = 3000;
+
+var app = express();
+
+// serve static content for the app from the "public" directory in the application
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// import routes and give the server access to them
+var routes = require("./controllers/burgers-controllers.js");
+
+app.use("/", routes);
+
+app.listen(port);
+console.log("app listening on port " + port);
